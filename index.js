c8845ee5b64e4df82573360d74c716a63cdc36ef
AmanatUMahmud <amanatmahmud@gmail.com>
2022-10-24 22:48:27 +0600

    courses.json(completed)

--- index.js
+++ index.js
@@ -2,11 +2,21 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
-
+const courses = require('./data/courses.json')
 app.use(cors());
 app.get('/', (req, res) => {
     res.send('Running');
 });
+app.get('/courses',(req,res)=>{
+    res.send(courses)
+})
+
+
+
+
+
+
+
 app.listen(port,()=>{
     console.log("Server running on",port);
 })
